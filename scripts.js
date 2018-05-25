91b3894ed45ff0ea9ac093750e28b9963d5eee5c
David Hopkins <32249730+DavidHop@users.noreply.github.com>
2018-05-24 20:24:22 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,37 @@
+
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
 $(document).ready(function(){
-  alert("Document is ready!");
+
+<script>
+  var $root = $('html, body');
+    $('.navbar-nav a').click(function() {
+  var href = $.attr(this, 'href');
+    if (href != undefined && href != '#') {
+  $root.animate({
+    scrollTop: $(href).offset().top
+    }, 500, function () {
+      window.location.hash = href;
+    });
+  }
+});
+</script>
+
+<script>
+ $(function () {
+   $('#item1').tooltip();
+   $('[data-toggle="tooltip"]').tooltip();
+ });
+</script>
+
+
+
+
 });
