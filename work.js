0dd40d6fe2e4b3a83081bac7055d3593462ac898
David Hopkins <32249730+DavidHop@users.noreply.github.com>
2018-09-28 09:24:07 -0400

    Update work.js

--- work.js
+++ work.js
@@ -1,6 +1,25 @@
 var works = [
-  "img/owl.jpg",
-  "img/chipmunk.jpg",
-  "img/goat.jpg",
-  "img/kitten.jpg"
-]
+  {
+    title = "First Project",
+    pic = "img/comingsoon.jpg"
+  },
+  {
+    title = "Second Project",
+    pic = "img/goat.jpg"
+  },
+  {
+    title = "Third Project",
+    pic = "img/comingsoon.jpg"
+  },
+  {
+    title = "Fourth Project",
+    pic = "img/comingsoon.jpg"
+  }
+];
+
+// var el = document.getElementById("images-container");
+//
+// works.forEach(function(img) {
+// 	/* console.log(img) */
+//   el.innerHTML += "<img class='work-img' src='" + img + "'>";
+// });
